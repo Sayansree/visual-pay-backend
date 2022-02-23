6bb50761f6f804a6754d5023eae891d0e249ec6c
Sayansree Paria <sayansreeparia@gmail.com>
2022-02-23 22:27:59 +0530

    redirect url

--- util/consent_detail.js
+++ util/consent_detail.js
@@ -43,7 +43,7 @@ const createData = (mobileNumber) => {
       },
       fiTypes: ["DEPOSIT"],
     },
-    redirectUrl: "<YOUR_REDIRECT_URL>",
+    redirectUrl: "https://visualpe.herokuapp.com/",
   });
 
   return data;
